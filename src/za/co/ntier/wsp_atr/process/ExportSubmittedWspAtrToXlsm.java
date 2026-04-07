9942a15d0a9db85b243c1c7d0b6ecb7185802121
Martin Augustine <maugustine@ntier.co.za>
2026-04-07 08:24:46 +0200

    Split export tabs at 1,000,000 rows including header

--- src/za/co/ntier/wsp_atr/process/ExportSubmittedWspAtrToXlsm.java
+++ src/za/co/ntier/wsp_atr/process/ExportSubmittedWspAtrToXlsm.java
@@ -1,15 +1,24 @@
 package za.co.ntier.wsp_atr.process;
 
 import org.adempiere.base.annotation.Process;
+import org.adempiere.base.annotation.Parameter;
 import org.apache.poi.ss.usermodel.Cell;
 import org.compiere.model.MColumn;
+import org.compiere.model.MProcessPara;
+import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 
 @Process(name = "za.co.ntier.wsp_atr.process.ExportSubmittedWspAtrToXlsm")
 public class ExportSubmittedWspAtrToXlsm extends SvrProcess {
 
+    @Parameter(name = "ZZ_FinYear_ID")
+    private int fiscalYearId;
+
     @Override
     protected void prepare() {
+        for (ProcessInfoParameter para : getParameter()) {
+            MProcessPara.validateUnknownParameter(getProcessInfo().getAD_Process_ID(), para);
+        }
     }
 
     @Override
@@ -26,5 +35,13 @@ public class ExportSubmittedWspAtrToXlsm extends SvrProcess {
 
     void writeResolvedValue(Cell cell, MColumn column, Object value) {
         new WspAtrExportValueFormatter(this).writeValue(cell, column, value);
+    }
+
+    int getFiscalYearId() {
+        return fiscalYearId;
+    }
+
+    boolean hasFiscalYearFilter() {
+        return fiscalYearId > 0;
     }
 }
