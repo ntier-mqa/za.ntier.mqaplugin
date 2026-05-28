ff29c8242638283fc016d5991aa84033c50fd487
martin <maugustine@ntier.co.za>
2026-05-28 10:51:12 +0200

    Check for report upload, not imported xls

--- src/za/co/ntier/wsp_atr/service/WspAtrUploadsService.java
+++ src/za/co/ntier/wsp_atr/service/WspAtrUploadsService.java
@@ -212,10 +212,10 @@ public class WspAtrUploadsService {
      //   }
 
        // boolean hasTemplate = true;
-        boolean hasTemplate = repo.hasSubmittedTemplateAttachment(submittedId);
-        //boolean hasReport = repo.hasUploadTypeAttachment(submittedId,
-          //      X_ZZ_WSP_ATR_Uploads.ZZ_WSP_ATR_UPLOAD_TYPE_UploadWSP_ATRReport);
-        return hasTemplate; // && hasReport;
+      //  boolean hasTemplate = repo.hasSubmittedTemplateAttachment(submittedId);
+        boolean hasReport = repo.hasUploadTypeAttachment(submittedId,
+                X_ZZ_WSP_ATR_Uploads.ZZ_WSP_ATR_UPLOAD_TYPE_UploadWSP_ATRReport);
+        return hasReport; //  &&hasTemplate &&
     }
 
     private boolean isChildWithParentUploadsEnabled(Properties ctx, int submittedId, String trxName) {
