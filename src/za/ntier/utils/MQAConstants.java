f519e428f626cf491f71eaa2c1ea908949fdb776
niraj <nmangukiya@logilite.com>
2026-06-11 10:40:18 +0530

    MQA-317: Enable zoom for Assessor-Moderator view based info window

--- src/za/ntier/utils/MQAConstants.java
+++ src/za/ntier/utils/MQAConstants.java
@@ -14,7 +14,8 @@ public final class MQAConstants {
      */
     public static final String[] CUSTOM_ZOOM_TABLES = {
         "ZZ_QCTO_APPLICATION_INFO_V",
-        "ZZ_AuditSchedule_v"
+        "ZZ_AuditSchedule_v",
+        "ZZAssessorPerson_v"
     };
 
     /**
