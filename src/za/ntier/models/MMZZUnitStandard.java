f4503262ccfb46afb8807d96be102fe1a59a6734
niraj <nmangukiya@logilite.com>
2026-07-22 13:13:13 +0530

    MQA-349: validate last enrollment/achievement dates against registration start date

---
+++ src/za/ntier/models/MMZZUnitStandard.java
@@ -0,0 +1,63 @@
+package za.ntier.models;
+
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Properties;
+
+import za.co.ntier.api.model.X_ZZUnitStandard;
+
+@SuppressWarnings("serial")
+public class MMZZUnitStandard extends X_ZZUnitStandard
+{
+
+	public MMZZUnitStandard(Properties ctx, String ZZUnitStandard_UU, String trxName, String[] virtualColumns)
+	{
+		super(ctx, ZZUnitStandard_UU, trxName, virtualColumns);
+	}
+
+	public MMZZUnitStandard(Properties ctx, String ZZUnitStandard_UU, String trxName)
+	{
+		super(ctx, ZZUnitStandard_UU, trxName);
+	}
+
+	public MMZZUnitStandard(Properties ctx, ResultSet rs, String trxName)
+	{
+		super(ctx, rs, trxName);
+	}
+
+	public MMZZUnitStandard(Properties ctx, int ZZUnitStandard_ID, String trxName, String[] virtualColumns)
+	{
+		super(ctx, ZZUnitStandard_ID, trxName, virtualColumns);
+	}
+
+	public MMZZUnitStandard(Properties ctx, int ZZUnitStandard_ID, String trxName)
+	{
+		super(ctx, ZZUnitStandard_ID, trxName);
+	}
+	
+	@Override
+	protected boolean beforeSave(boolean newRecord)
+	{
+		Timestamp regStartDate = getRegistrationstartdate();
+		if (regStartDate != null)
+		{
+			Timestamp lastEnrolmentDate = getZZLastEnrolmentDate();
+			if (lastEnrolmentDate != null && lastEnrolmentDate.before(regStartDate))
+			{
+				log.saveError("Error", "Last enrollment date cannot be before Registration start date");
+				return false;
+			}
+
+			Timestamp lastAchievementDate = getZZLastAchievementDate();
+			if (lastAchievementDate != null && lastAchievementDate.before(regStartDate))
+			{
+				log.saveError("Error", "Last achievement date cannot be before Registration start date");
+				return false;
+			}
+		}
+
+		return super.beforeSave(newRecord);
+	}
+
+}
+
--- src/za/ntier/models/MyModelFactory.java
+++ src/za/ntier/models/MyModelFactory.java
@@ -14,6 +14,7 @@ import org.osgi.service.component.annotations.Component;
 
 import za.co.ntier.api.model.I_ZZAssessorPerson;
 import za.co.ntier.api.model.I_ZZSdfOrganisation;
+import za.co.ntier.api.model.I_ZZUnitStandard;
 import za.co.ntier.api.model.I_ZZ_Program_Master_Data;
 import za.co.ntier.api.model.I_ZZ_WSP_ATR_EXTENSION_BATCH;
 import za.co.ntier.api.model.X_ZZAssessorPerson;
@@ -102,6 +103,7 @@ public class MyModelFactory implements IModelFactory {
         register(I_ZZ_WSP_ATR_EXTENSION_BATCH.Table_Name,MZZWSPATRExtensionBatch.class);
         register(I_ZZAssessorPerson.Table_Name, X_ZZAssessorPerson.class);
         register(I_ZZ_WF_Next_Node.Table_Name, X_ZZ_WF_Next_Node.class);
+        register(I_ZZUnitStandard.Table_Name, MMZZUnitStandard.class);
     }
 
     private static void register(String tableName, Class<? extends PO> modelClass) {
